655db71fc32a4daa362355cfe23bb0e8b78941eb
Sukhdeep singh <Sukhdeepsingh141414@gmail.com>
2025-01-26 19:46:50 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,40 @@
+public class Hello {
+    public static void main(String[] args) {
+        // Java has 8 primitive data types: Boolean, char, byte, int, short, long,
+        // float, and double
+
+        // byte - 1 [-128, 127]
+        byte age = 50;
+
+        // short - 2
+        short num = 1;
+
+        // int - 4
+        int phone = 814138291;
+
+        // long - 8
+        long phoneOne = 7814138291L;
+
+        // float - 4
+        float moneny = 6454.455F;
+
+        // double - 8
+        double longNum = 188.999D;
+
+        // char - 2
+        char letter = 'Q';
+
+        // boolean - 1 [true / flase]
+        boolean isAdult = true;
+
+        System.out.println(letter);
+        System.out.println(isAdult);
+        System.out.println(longNum);
+        System.out.println(moneny);
+        System.out.println(phoneOne);
+        System.out.println(phone);
+        System.out.println(age);
+        System.out.println(num);
+
+    }
+}
